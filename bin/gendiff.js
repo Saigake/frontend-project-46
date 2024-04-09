814dd40984b1c93ecc7d946a1c9289975caa24d9
Alex Fedorov <Alexandr.Fedorov00@gmail.com>
2024-04-09 20:33:42 +0400

    Step_2.0

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-const program = new Command();
+import { program } from 'commander';
+const program = new program();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0')
   .option('-V, --version', 'output the version number')
-  .helpOption('-e, --HELP', 'output usage information');
+  .helpOption('-e, --HELP', 'output usage information')
+  .option(
+    '-f, --format [type]',
+    'output format: plain, stylish, json',
+    'stylish'
+  )
+  .argument('<filepath1>', '<filepath2>')
+  .action((filepath1, filepath2) => {
+    const options = program.opts().format;
+    const result = gendiff(filepath1, filepath2, options);
+    console.log(result);
+  });
 
 program.parse();
